1474ed097509730716788d7c67b5a69d88726314
Szymon Banka <sbanka@mirantis.com>
2015-03-19 12:38:07 +0100

    add Zabbix plugin code
    Change-Id: I9e1c1073453a4f49f660ad992df1e861c6e45373

---
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/frontend.pp
@@ -0,0 +1,39 @@
+class plugin_zabbix::frontend {
+
+  include plugin_zabbix::params
+
+  service { $plugin_zabbix::params::frontend_service:
+    ensure     => 'running',
+    name       => $plugin_zabbix::params::frontend_service,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+  }
+
+  package { $plugin_zabbix::params::frontend_pkg:
+    ensure    => present,
+    before    => [ File[$plugin_zabbix::params::frontend_config], File[$plugin_zabbix::params::frontend_php_ini] ],
+  }
+
+  file { $plugin_zabbix::params::frontend_config:
+    ensure    => present,
+    content   => template($plugin_zabbix::params::frontend_config_template),
+    notify    => Service[$plugin_zabbix::params::frontend_service],
+  }
+
+  file { $plugin_zabbix::params::frontend_php_ini:
+    ensure    => present,
+    content   => template($plugin_zabbix::params::frontend_php_ini_template),
+    notify    => Service[$plugin_zabbix::params::frontend_service],
+  }
+
+  if $::osfamily == 'RedHat' {
+    file_line { 'httpd_mpm_prefork':
+      path  => '/etc/sysconfig/httpd',
+      line  => 'HTTPD=/usr/sbin/httpd',
+      match => '^HTTPD=/usr/sbin/httpd',
+      notify => Service[$plugin_zabbix::params::frontend_service],
+      require => Package[$plugin_zabbix::params::frontend_pkg],
+    }
+  }
+}
---
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/monitoring/rabbitmq_mon.pp
@@ -0,0 +1,46 @@
+class plugin_zabbix::monitoring::rabbitmq_mon {
+
+  include plugin_zabbix::params
+
+  if $::fuel_settings['deployment_mode'] == 'multinode' {
+    $template = 'Template App OpenStack RabbitMQ'
+  } else {
+    $template = 'Template App OpenStack HA RabbitMQ'
+  }
+
+  #RabbitMQ server
+  if defined_in_state(Class['rabbitmq::server']) {
+
+    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template App OpenStack RabbitMQ":
+      host     => $plugin_zabbix::params::host_name,
+      template => $template,
+      api      => $plugin_zabbix::monitoring::api_hash,
+    }
+
+    exec { 'enable rabbitmq management plugin':
+      command     => 'rabbitmq-plugins enable rabbitmq_management',
+      path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
+      unless      => 'rabbitmq-plugins list -m -E rabbitmq_management | grep -q rabbitmq_management',
+      environment => 'HOME=/root',
+    }
+
+    firewall {'992 rabbitmq management':
+      port   => '15672',
+      proto  => 'tcp',
+      action => 'accept',
+    }
+
+    plugin_zabbix::agent::userparameter {
+      'rabbitmq.queue.items':
+        command => '/etc/zabbix/scripts/check_rabbit.py queues-items';
+      'rabbitmq.queues.without.consumers':
+        command => '/etc/zabbix/scripts/check_rabbit.py queues-without-consumers';
+      'rabbitmq.missing.nodes':
+        command => '/etc/zabbix/scripts/check_rabbit.py missing-nodes';
+      'rabbitmq.unmirror.queues':
+        command => '/etc/zabbix/scripts/check_rabbit.py unmirror-queues';
+      'rabbitmq.missing.queues':
+        command => '/etc/zabbix/scripts/check_rabbit.py missing-queues';
+    }
+  }
+}
