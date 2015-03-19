0d60e55f4721b25b596dad3b97ab05276a211535
Szymon Banka <sbanka@mirantis.com>
2015-03-19 12:38:07 +0100

    add Zabbix plugin code
    Change-Id: I9e1c1073453a4f49f660ad992df1e861c6e45373

---
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/monitoring/keystone_mon.pp
@@ -0,0 +1,24 @@
+class plugin_zabbix::monitoring::keystone_mon {
+
+  include plugin_zabbix::params
+
+  #Keystone
+  if defined_in_state(Class['keystone']) {
+    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template App OpenStack Keystone":
+      host => $plugin_zabbix::params::host_name,
+      template => 'Template App OpenStack Keystone',
+      api => $plugin_zabbix::monitoring::api_hash,
+    }
+    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template App OpenStack Keystone API check":
+      host    => $plugin_zabbix::params::host_name,
+      template => 'Template App OpenStack Keystone API check',
+      api => $plugin_zabbix::monitoring::api_hash,
+    }
+    plugin_zabbix::agent::userparameter {
+      'keystone.api.status':
+        command => "/etc/zabbix/scripts/check_api.py keystone http ${::internal_address} 5000";
+      'keystone.service.api.status':
+        command => "/etc/zabbix/scripts/check_api.py keystone_service http ${::internal_address} 35357";
+    }
+  }
+}
