93f5ac71be51a82b6e2f0f0fdb3766b1a267aa4f
Szymon Banka <sbanka@mirantis.com>
2015-03-19 12:38:07 +0100

    add Zabbix plugin code
    Change-Id: I9e1c1073453a4f49f660ad992df1e861c6e45373

---
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/monitoring/rabbitmq_mon.pp
@@ -0,0 +1,55 @@
+#
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+class plugin_zabbix::monitoring::rabbitmq_mon {
+
+  include plugin_zabbix::params
+
+  #RabbitMQ server
+  if defined_in_state(Class['Rabbitmq']) {
+
+    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template App OpenStack RabbitMQ":
+      host        => $plugin_zabbix::params::host_name,
+      template    => 'Template App OpenStack HA RabbitMQ',
+      api         => $plugin_zabbix::monitoring::api_hash,
+    }
+
+    exec { 'enable rabbitmq management plugin':
+      command     => 'rabbitmq-plugins enable rabbitmq_management',
+      path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
+      unless      => 'rabbitmq-plugins list -m -E rabbitmq_management | grep -q rabbitmq_management',
+      environment => 'HOME=/root',
+    }
+
+    firewall {'992 rabbitmq management':
+      port        => '15672',
+      proto       => 'tcp',
+      action      => 'accept',
+    }
+
+    plugin_zabbix::agent::userparameter {
+      'rabbitmq.queue.items':
+        command   => '/etc/zabbix/scripts/check_rabbit.py queues-items';
+      'rabbitmq.queues.without.consumers':
+        command   => '/etc/zabbix/scripts/check_rabbit.py queues-without-consumers';
+      'rabbitmq.missing.nodes':
+        command   => '/etc/zabbix/scripts/check_rabbit.py missing-nodes';
+      'rabbitmq.unmirror.queues':
+        command   => '/etc/zabbix/scripts/check_rabbit.py unmirror-queues';
+      'rabbitmq.missing.queues':
+        command   => '/etc/zabbix/scripts/check_rabbit.py missing-queues';
+    }
+  }
+}
---
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/primary_controller.pp
@@ -0,0 +1,62 @@
+#
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+class plugin_zabbix::primary_controller {
+
+  include plugin_zabbix::controller
+
+  class { 'plugin_zabbix::db':
+    db_ip       => $plugin_zabbix::params::db_ip,
+    db_password => $plugin_zabbix::params::db_password,
+    require     => Package[$plugin_zabbix::params::server_pkg],
+    before      => [ Class['plugin_zabbix::frontend'], Cs_resource["p_${plugin_zabbix::params::server_service}"] ],
+  }
+
+  cs_resource { "p_${plugin_zabbix::params::server_service}":
+    primitive_class => 'ocf',
+    provided_by     => $plugin_zabbix::params::ocf_scripts_provider,
+    primitive_type  => $plugin_zabbix::params::server_service,
+    operations      => {
+      'monitor' => { 'interval' => '5s', 'timeout' => '30s' },
+      'start'   => { 'interval' => '0', 'timeout' => '30s' }
+    },
+    metadata        => {
+      'migration-threshold' => '3',
+      'failure-timeout'     => '120',
+    },
+  }
+
+  cs_colocation { 'vip_management-with-zabbix-server':
+    ensure     => present,
+    score      => 'INFINITY',
+    primitives => [
+        'vip__management',
+        "p_${plugin_zabbix::params::server_service}"
+    ],
+  }
+
+  service { "${plugin_zabbix::params::server_service}-started":
+    ensure    => running,
+    name      => "p_${plugin_zabbix::params::server_service}",
+    enable    => true,
+    provider  => 'pacemaker',
+  }
+
+  File[$plugin_zabbix::params::server_config] -> File['zabbix-server-ocf'] -> Cs_resource["p_${plugin_zabbix::params::server_service}"]
+  Service["${plugin_zabbix::params::server_service}-init-stopped"] -> Cs_resource["p_${plugin_zabbix::params::server_service}"]
+  Cs_resource["p_${plugin_zabbix::params::server_service}"] -> Cs_colocation['vip_management-with-zabbix-server']
+  Cs_resource["p_${plugin_zabbix::params::server_service}"] -> Service["${plugin_zabbix::params::server_service}-started"]
+
+}
