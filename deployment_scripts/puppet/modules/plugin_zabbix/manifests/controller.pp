4507c5a9dbc4d4732757fa1fa75984c6b068b72d
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-09-01 03:57:09 +0300

    add php-mysql dependency
    Change-Id: I94c78331bf8e6f394dcb081266d68b25034c356d
    Closes-Bug: #1491159

--- deployment_scripts/puppet/modules/plugin_zabbix/manifests/controller.pp
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/controller.pp
@@ -34,7 +34,14 @@ class plugin_zabbix::controller {
 
   package { $plugin_zabbix::params::server_pkg:
     ensure    => present,
-    require   => File['/etc/dbconfig-common/zabbix-server-mysql.conf'],
+    require   => [
+      File['/etc/dbconfig-common/zabbix-server-mysql.conf'],
+      Package[$plugin_zabbix::params::php_mysql_pkg]
+    ],
+  }
+
+  package { $plugin_zabbix::params::php_mysql_pkg:
+    ensure    => present
   }
 
   file { $plugin_zabbix::params::server_config:
