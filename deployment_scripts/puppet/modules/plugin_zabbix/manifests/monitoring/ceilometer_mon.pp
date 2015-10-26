d43cc92dfa7da0716c0524d367e3db742c33a88f
Bartosz Kupidura <bkupidura@mirantis.com>
2015-10-26 15:57:09 +0100

    Fix old class names for neutron Add template for nova-conductor Fix cinder-volume trigger
    Change-Id: I3bba16d72eb4ca29c2a3a23a26c47f600f346f21
    Closes-Bug: 1446257

--- deployment_scripts/puppet/modules/plugin_zabbix/manifests/monitoring/ceilometer_mon.pp
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/monitoring/ceilometer_mon.pp
@@ -18,7 +18,7 @@ class plugin_zabbix::monitoring::ceilometer_mon {
   include plugin_zabbix::params
 
   #Ceilometer
-  if defined_in_state(Class['Openstack::Ceilometer']) and defined_in_state(Class['Openstack::Controller']) {
+  if defined_in_state(Class['Ceilometer']) and defined_in_state(Class['Openstack::Controller']) {
     plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template App OpenStack Ceilometer":
       host => $plugin_zabbix::params::host_name,
       template => 'Template App OpenStack Ceilometer',
