9ab8c4ef8281a82d52833bc60aaed64ec069d523
Swann Croiset <scroiset@mirantis.com>
2015-11-06 14:48:45 +0100

    Add check for ntpd binding on UDP port 123
    This check is performed on all nodes except for controller nodes.
    
    Related-bug: #1513454
    
    Change-Id: Iecf14599a01e66b35e57b5b7e656a4329545f39d

--- deployment_scripts/puppet/modules/plugin_zabbix/manifests/server/config.pp
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/server/config.pp
@@ -50,6 +50,12 @@ class plugin_zabbix::server::config {
     api      => $api_hash,
   }
 
+  plugin_zabbix_configuration_import { 'Template_NTP_binding Import':
+    ensure   => present,
+    xml_file => '/etc/zabbix/import/Template_NTP_binding.xml',
+    api      => $api_hash,
+  }
+
   # Nova templates
   plugin_zabbix_configuration_import { 'Template_App_OpenStack_Nova_API_EC2.xml Import':
     ensure   => present,
