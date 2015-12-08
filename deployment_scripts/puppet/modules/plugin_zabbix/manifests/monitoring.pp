5f57443e4b4b3afe64bd985fd5595ecbe1616760
Olivier Bourdon <obourdon@mirantis.com>
2015-12-08 17:51:47 +0100

    Fix deployment of base-os node
    Change-Id: Ia83095b301fbdee8e038eca5373c894c1c4be10f
    Closes-Bug: #1515956

--- deployment_scripts/puppet/modules/plugin_zabbix/manifests/monitoring.pp
+++ deployment_scripts/puppet/modules/plugin_zabbix/manifests/monitoring.pp
@@ -18,119 +18,127 @@ class plugin_zabbix::monitoring(
   $roles = [],
 ) {
 
-  validate_array($roles)
+  # This should evaluate to false on base-os only nodes
+  # and therefore Zabbix should not be installed neither configured in this case
+  $zabbix_agent_not_supported = member($roles, 'base-os') and (size($roles) == 1)
 
-  include plugin_zabbix::params
+  if $zabbix_agent_not_supported {
+    notice('Skipping Zabbix configuration for base-os only host')
+  } else {
+    validate_array($roles)
 
-  $api_hash = $plugin_zabbix::params::api_hash
+    include plugin_zabbix::params
 
-  if !defined(Package['snmp-mibs-downloader']) {
-    package { 'snmp-mibs-downloader':
-      ensure => 'present',
+    $api_hash = $plugin_zabbix::params::api_hash
+
+    if !defined(Package['snmp-mibs-downloader']) {
+      package { 'snmp-mibs-downloader':
+        ensure => 'present',
+      }
     }
-  }
 
-  if is_ip_address($::public_address) {
-    plugin_zabbix_usermacro { "${plugin_zabbix::params::host_name} IP_PUBLIC":
-      host  => $plugin_zabbix::params::host_name,
-      macro => '{$IP_PUBLIC}',
-      value => $::public_address,
-      api   => $api_hash,
+    if is_ip_address($::public_address) {
+      plugin_zabbix_usermacro { "${plugin_zabbix::params::host_name} IP_PUBLIC":
+        host  => $plugin_zabbix::params::host_name,
+        macro => '{$IP_PUBLIC}',
+        value => $::public_address,
+        api   => $api_hash,
+      }
     }
-  }
 
-  if is_ip_address($::internal_address) {
-    plugin_zabbix_usermacro { "${plugin_zabbix::params::host_name} IP_MANAGEMENT":
-      host  => $plugin_zabbix::params::host_name,
-      macro => '{$IP_MANAGEMENT}',
-      value => $::internal_address,
-      api   => $api_hash,
+    if is_ip_address($::internal_address) {
+      plugin_zabbix_usermacro { "${plugin_zabbix::params::host_name} IP_MANAGEMENT":
+        host  => $plugin_zabbix::params::host_name,
+        macro => '{$IP_MANAGEMENT}',
+        value => $::internal_address,
+        api   => $api_hash,
+      }
     }
-  }
 
-  if is_ip_address($::swift_address) {
-    plugin_zabbix_usermacro { "${plugin_zabbix::params::host_name} IP_STORAGE":
-      host  => $plugin_zabbix::params::host_name,
-      macro => '{$IP_STORAGE}',
-      value => $::swift_address,
-      api   => $api_hash,
+    if is_ip_address($::swift_address) {
+      plugin_zabbix_usermacro { "${plugin_zabbix::params::host_name} IP_STORAGE":
+        host  => $plugin_zabbix::params::host_name,
+        macro => '{$IP_STORAGE}',
+        value => $::swift_address,
+        api   => $api_hash,
+      }
     }
-  }
 
-  class { 'plugin_zabbix::agent':
-    api_hash => $api_hash,
-    before   => Class['plugin_zabbix::agent::scripts'],
-  }
+    class { 'plugin_zabbix::agent':
+      api_hash => $api_hash,
+      before   => Class['plugin_zabbix::agent::scripts'],
+    }
 
-  class { 'plugin_zabbix::agent::scripts': }
+    class { 'plugin_zabbix::agent::scripts': }
 
-  plugin_zabbix::agent::userparameter {
-    'vfs.dev.discovery':
-      ensure  => 'present',
-      command => '/etc/zabbix/scripts/vfs.dev.discovery.sh';
-    'vfs.mdadm.discovery':
-      ensure  => 'present',
-      command => '/etc/zabbix/scripts/vfs.mdadm.discovery.sh';
-    'proc.vmstat':
-      key     => 'proc.vmstat[*]',
-      command => 'grep \'$1\' /proc/vmstat | awk \'{print $$2}\'';
-    'crm.node.check':
-      key     => 'crm.node.check[*]',
-      command => '/etc/zabbix/scripts/crm_node_check.sh $1';
-    'netns.udp.listen':
-      key     => 'netns.udp.listen[*]',
-      command => '/usr/bin/sudo /etc/zabbix/scripts/netns.listen.sh UDP $1 $2 $3';
-  }
+    plugin_zabbix::agent::userparameter {
+      'vfs.dev.discovery':
+        ensure  => 'present',
+        command => '/etc/zabbix/scripts/vfs.dev.discovery.sh';
+      'vfs.mdadm.discovery':
+        ensure  => 'present',
+        command => '/etc/zabbix/scripts/vfs.mdadm.discovery.sh';
+      'proc.vmstat':
+        key     => 'proc.vmstat[*]',
+        command => 'grep \'$1\' /proc/vmstat | awk \'{print $$2}\'';
+      'crm.node.check':
+        key     => 'crm.node.check[*]',
+        command => '/etc/zabbix/scripts/crm_node_check.sh $1';
+      'netns.udp.listen':
+        key     => 'netns.udp.listen[*]',
+        command => '/usr/bin/sudo /etc/zabbix/scripts/netns.listen.sh UDP $1 $2 $3';
+    }
 
-  #Linux
-  plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template Fuel OS Linux":
-    host     => $plugin_zabbix::params::host_name,
-    template => 'Template Fuel OS Linux',
-    api      => $api_hash,
-  }
-
-  if ! member($roles, 'controller') and ! member($roles, 'primary-controller') {
-    # default way to check NTP binding
-    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template NTP binding":
+    #Linux
+    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template Fuel OS Linux":
       host     => $plugin_zabbix::params::host_name,
-      template => 'Template NTP binding',
+      template => 'Template Fuel OS Linux',
       api      => $api_hash,
     }
-  }else{
-    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template OS Controller":
+
+    if ! member($roles, 'controller') and ! member($roles, 'primary-controller') {
+      # default way to check NTP binding
+      plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template NTP binding":
+        host     => $plugin_zabbix::params::host_name,
+        template => 'Template NTP binding',
+        api      => $api_hash,
+      }
+    }else{
+      plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template OS Controller":
+        host     => $plugin_zabbix::params::host_name,
+        template => 'Template OS Controller',
+        api      => $api_hash,
+      }
+    }
+
+    #Zabbix Agent
+    plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template App Zabbix Agent":
       host     => $plugin_zabbix::params::host_name,
-      template => 'Template OS Controller',
+      template => 'Template App Zabbix Agent',
       api      => $api_hash,
     }
+
+    Plugin_zabbix_usermacro { require => Class['plugin_zabbix::agent'] }
+    Plugin_zabbix_template_link { require => Class['plugin_zabbix::agent'] }
+
+    # Auto-registration
+    include plugin_zabbix::monitoring::nova_mon
+    include plugin_zabbix::monitoring::keystone_mon
+    include plugin_zabbix::monitoring::glance_mon
+    include plugin_zabbix::monitoring::cinder_mon
+    include plugin_zabbix::monitoring::swift_mon
+    include plugin_zabbix::monitoring::rabbitmq_mon
+    include plugin_zabbix::monitoring::horizon_mon
+    include plugin_zabbix::monitoring::mysql_mon
+    include plugin_zabbix::monitoring::memcached_mon
+    include plugin_zabbix::monitoring::haproxy_mon
+    include plugin_zabbix::monitoring::zabbixserver_mon
+    include plugin_zabbix::monitoring::openstack_virtual_mon
+    include plugin_zabbix::monitoring::neutron_mon
+    include plugin_zabbix::monitoring::openvswitch_mon
+    include plugin_zabbix::monitoring::ceilometer_mon
+    include plugin_zabbix::monitoring::ceilometer_compute_mon
+    include plugin_zabbix::monitoring::ceph_mon
+    include plugin_zabbix::monitoring::firewall_mon
   }
-
-  #Zabbix Agent
-  plugin_zabbix_template_link { "${plugin_zabbix::params::host_name} Template App Zabbix Agent":
-    host     => $plugin_zabbix::params::host_name,
-    template => 'Template App Zabbix Agent',
-    api      => $api_hash,
-  }
-
-  Plugin_zabbix_usermacro { require => Class['plugin_zabbix::agent'] }
-  Plugin_zabbix_template_link { require => Class['plugin_zabbix::agent'] }
-
-  # Auto-registration
-  include plugin_zabbix::monitoring::nova_mon
-  include plugin_zabbix::monitoring::keystone_mon
-  include plugin_zabbix::monitoring::glance_mon
-  include plugin_zabbix::monitoring::cinder_mon
-  include plugin_zabbix::monitoring::swift_mon
-  include plugin_zabbix::monitoring::rabbitmq_mon
-  include plugin_zabbix::monitoring::horizon_mon
-  include plugin_zabbix::monitoring::mysql_mon
-  include plugin_zabbix::monitoring::memcached_mon
-  include plugin_zabbix::monitoring::haproxy_mon
-  include plugin_zabbix::monitoring::zabbixserver_mon
-  include plugin_zabbix::monitoring::openstack_virtual_mon
-  include plugin_zabbix::monitoring::neutron_mon
-  include plugin_zabbix::monitoring::openvswitch_mon
-  include plugin_zabbix::monitoring::ceilometer_mon
-  include plugin_zabbix::monitoring::ceilometer_compute_mon
-  include plugin_zabbix::monitoring::ceph_mon
-  include plugin_zabbix::monitoring::firewall_mon
 }
