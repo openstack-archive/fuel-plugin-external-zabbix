488218c3f23dacf5a1c50caf79037e24a4a66518
Olivier Bourdon <obourdon@mirantis.com>
2016-01-27 15:38:03 +0100

    Refactor in prevision of templating of XML imported files
    Change-Id: I68436d2ffef21d7d47cb7c3bc564a279fc6c8c8f

---
+++ deployment_scripts/puppet/modules/plugin_zabbix/spec/defines/plugin_zabbix_server_zabbix_import_xmltemplate_spec.rb
@@ -0,0 +1,44 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+require 'spec_helper'
+
+describe 'plugin_zabbix::server::zabbix_import_xmltemplate', :type => :define  do
+
+  let (:facts) do
+    {
+      :osfamily        => 'Debian',
+      :operatingsystem => 'Ubuntu',
+      :kernel          => 'Linux',
+      :memoryfree_mb   => 1024,
+    }
+  end
+
+  let (:title) { 'zabbix_import_xmltemplate' }
+
+  let (:default_params) do
+    {
+        :ensure => 'present',
+    }
+  end
+
+  context 'with defaults' do
+    it { should compile }
+    it 'should contain plugin_zabbix::server::zabbix_import_xmltemplate definition' do
+      is_expected.to contain_plugin_zabbix__server__zabbix_import_xmltemplate(title).with(default_params)
+    end
+  end
+
+#  describe 'with defaults' do
+#  end
+end
